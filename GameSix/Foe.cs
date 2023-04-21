c1508d3eb0aebe08958886be16a79a114ee59d37
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2023-04-21 11:26:14 +0100

    add constructors

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,7 +2,17 @@ namespace GameSix
 {
     public class Foe
     {
-
+        private string name;
+        private float health;
+        private float shield;
+        
+        // Construtor
+        public Foe (string name)
+        {
+            this.name = name; // name oculta this.name
+            health = 100;
+            shield = 0 ; 
+        }
     }
-    
+
 }
