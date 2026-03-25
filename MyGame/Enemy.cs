8c7004018b5122091bde1c44aaffbdb9ed308452
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-03-25 18:25:10 +0000

    Update class Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,7 +1,20 @@
+using System.Runtime.CompilerServices;
+
 namespace MyGame
 {
     public class Enemy
     {
-        
+       
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy (string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+         
     }
 }
--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,7 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            
         }
     }
 }
