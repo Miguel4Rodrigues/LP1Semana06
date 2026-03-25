7cf6e37de99f6d2781c20a3ff2061541ac718109
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-03-25 15:58:22 +0000

    Update RandDice

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,26 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Uso: RandDice <n> <s>");
+                return;
+            }
+
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            Random rand = new Random(s);
+
+            int soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int valor = rand.Next(1, 7);
+                soma += valor;
+            }
+
+            Console.WriteLine(soma);
         }
     }
 }
